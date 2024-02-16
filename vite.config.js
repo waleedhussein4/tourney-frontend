2485cd9ca23abfb8bdd69a931ffca8e2aabaeff3
Waleed <walid.a.hussein@hotmail.com>
2024-02-16 13:23:55 +0200

    add pages to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import path from 'path'
 import react from '@vitejs/plugin-react'
+
+const __dirname = path.dirname("./src")
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        team: resolve(__dirname, 'pages/team/team.html'),
+        tournament: resolve(__dirname, 'pages/tournament/tournament.html'),
+        tournaments: resolve(__dirname, 'pages/tournaments/tournaments.html')
+      }
+    }
+  }
 })
