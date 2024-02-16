ebf20154a1eddffeddedf4c29feae1df697f02bf
Waleed <walid.a.hussein@hotmail.com>
2024-02-16 13:34:51 +0200

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,23 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
-import path from 'path'
 import react from '@vitejs/plugin-react'
 
-const __dirname = path.dirname("./src")
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './',
   build: {
+    outDir,
+    emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        team: resolve(__dirname, 'pages/team/team.html'),
-        tournament: resolve(__dirname, 'pages/tournament/tournament.html'),
-        tournaments: resolve(__dirname, 'pages/tournaments/tournaments.html')
+        main: resolve(root, 'index.html'),
+        team: resolve(root, 'pages/team/team.html'),
+        tournament: resolve(root, 'pages/tournament/tournament.html'),
+        tournaments: resolve(root, 'pages/tournaments/tournaments.html')
       }
     }
   }
