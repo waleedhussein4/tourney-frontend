7829d927c1b6d98505df029a74ab475e6385323f
Waleed <walid.a.hussein@hotmail.com>
2024-02-21 20:15:04 +0200

    add main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './styles/App.css'
 
 import Nav from './components/Nav'
+import Main from './components/Main'
 
 function App() {
   return (
     <>
       <Nav></Nav>
+      <Main></Main>
     </>
   )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+import '../styles/Main.css'
+
+function Main() {
+  return (
+    <div id="main">
+      <div className="banner">
+        <img src="" alt="" />
+      </div>
+      <div className="banner">
+        <img src="" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Main
+
