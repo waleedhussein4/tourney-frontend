51d82e2ab38404596f304edd66ea970dc8489183
Waleed <walid.a.hussein@hotmail.com>
2024-03-02 01:07:39 +0200

    update Sidebar.jsx add category filter dropdown menu

--- tournaments/components/Sidebar.jsx
+++ tournaments/components/Sidebar.jsx
@@ -8,13 +8,31 @@ function Sidebar() {
         <input id='searchbar' type="search" placeholder='ID, title...' name='search' />
         <button type='submit'><img className='search_icon' src={search_icon} alt="" /></button>
       </form>
-      <span>Filters</span>
-      <ul>
-        <li>category</li>
-        <li>Entry fee</li>
-        <li>tournament type</li>
-        <li>public/private</li>
-      </ul>
+      <span className='filters-header'>Filters</span>
+      <div id="filters">
+        <div className="filter dropdown" data-name="category">
+          <span className="name">Category</span>
+          <div className="select">
+              <span className="selected">Fortnite</span>
+              <div className="imgContainer">
+                <img src="/src/assets/menu-down.svg" alt="" />
+              </div>
+          </div>
+          <ul className='menu category'>
+            <li data-value="Coding">Coding</li>
+            <li data-value="League of Legends">League of Legends</li>
+            <li data-value="Poker">Poker</li>
+            <li data-value="Chess">Chess</li>
+            <li data-value="Fortnite" className="active">Fortnite</li>
+            <li data-value="Football">Football</li>
+          </ul>
+        </div>
+        <ul>
+          <li>Entry fee</li>
+          <li>tournament type</li>
+          <li>public/private</li>
+        </ul>
+      </div>
     </div>
   )
 }
