55c37ede77513ac3b710dfb3a48533b1e9ad4dc8
Waleed <walid.a.hussein@hotmail.com>
2024-03-02 01:05:27 +0200

    update tournaments/App.jsx add Content component add category filter functionality

--- tournaments/App.jsx
+++ tournaments/App.jsx
@@ -1,15 +1,52 @@
 import '/src/styles/App.css'
+import './styles/App.css'
 
 import Nav from '/src/components/Nav'
 import Sidebar from './components/Sidebar.jsx'
+import Content from './components/Content.jsx'
 
 function App() {
   return (
     <>
       <Nav></Nav>
       <Sidebar></Sidebar>
+      <Content></Content>
     </>
   )
 }
 
 export default App
+
+window.addEventListener('load', dropdownHandler)
+
+function dropdownHandler() {
+  let dropdowns = document.querySelectorAll(".dropdown")
+
+  Array.from(dropdowns).forEach(dropdown => {
+    let name = dropdown.dataset.name
+    dropdown.addEventListener('click', () => { toggledropdown(name) } )
+
+    Array.from(dropdown.querySelectorAll('.menu li')).forEach(option => {
+      option.addEventListener('click', () => {
+        let value = option.dataset.value
+        let selected = dropdown.querySelector('.selected')
+        let active = dropdown.querySelector('.active')
+        active.classList.remove('active')
+        option.classList.add('active')
+        selected.innerText = value
+      })
+    })
+  })
+}
+
+function toggledropdown(name) {
+  let menu = document.querySelector(".menu." + name)
+  let display = window.getComputedStyle(menu).display
+  if(display == "none") {
+    menu.style.display = "block"
+  }
+  else {
+    menu.style.display = "none"
+  }
+}
+
