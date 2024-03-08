239cd2e5a69328b8da65644eb0b1db2b5888981d
Waleed <walid.a.hussein@hotmail.com>
2024-03-08 14:04:25 +0200

    add rendering of fetched data

--- tournaments/components/Content.jsx
+++ tournaments/components/Content.jsx
@@ -1,17 +1,45 @@
+/* eslint-disable react/prop-types */
+import { createRoot } from 'react-dom/client';
 import Tournament from './Tournament';
+import { useCallback, useEffect } from 'react';
 
-function Content(props) {
+const URL = ' https://api.npoint.io/62625fd6706d67650348'
 
-  const arrayDataItems = props.data.map((tourney) => <Tournament key={tourney.UUID} obj={tourney} />)
+function Content({tournaments, setTournaments}) {
+  
+  useEffect(() => {
+    renderTournaments(tournaments)
+  },[tournaments])
+
+  const fetchData = useCallback(async () => {
+    const response = await fetch(URL)
+    .then(res => res.json())
+    .then(data => setTournaments(data))
+    return response
+  }, [])
+
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   return (
     <div id="content">
       <h1>Tournaments</h1>
-      <div id="tournaments">
-        {arrayDataItems}
-      </div>
+      <div id="tournaments"></div>
     </div>
   )
+}
+
+function renderTournaments(tournaments) {
+
+  if(tournaments == undefined) return
+
+  const arrayDataItems = tournaments.map((tourney) => <Tournament key={tourney.UUID} obj={tourney} />)
+  let tournamentsContainer = document.getElementById('tournaments')
+
+  const domNode = tournamentsContainer
+  const root = createRoot(domNode)
+  root.render(arrayDataItems)
 }
 
 export default Content
