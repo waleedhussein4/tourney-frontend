b269faed9105027d1253e4d84f9f79999dfcfaa2
jadzeid <jadzeid004@gmail.com>
2024-03-16 20:28:30 +0200

    hi

--- signin/App.jsx
+++ signin/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 import Login from './Login'
 import '/src/styles/index.css'
+import  authContext  from './authContext'
 
 function App() {
   return (
+    <authContext>
     <Login />
+    </authContext>
   )
 }
 
