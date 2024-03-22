d59b34cdc6766d0c4ca26cb402049cf3a5be1ab3
Waleed <walid.a.hussein@hotmail.com>
2024-03-23 01:39:35 +0200

    update tournament page frontend create join tournament popups (lacking functionality)

--- client/src/pages/tournament/Tournament.jsx
+++ client/src/pages/tournament/Tournament.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./styles/App.css";
 import Nav from "/src/components/Nav.jsx";
+import alpha_x from '/src/assets/alpha-x.svg';
 
 const url = 'https://api.npoint.io/c9523c0ef25065fec8c2';
 const paramUUID = new URLSearchParams(window.location.search).get('UUID')
@@ -14,6 +15,13 @@ function Tournament() {
 
   const handleJoin = () => {
     console.log("Joining tournament");
+    displayJoinPopup()
+    // if(tournament.teamSize > 1) {
+    //   generateJoinAsTeamPopup()
+    // }
+    // else {
+    //   generateJoinAsSoloPopup()
+    // }
     // Implement joining logic here, e.g., update backend, redirect to 'join tournament' page
   };
 
@@ -45,6 +53,14 @@ function Tournament() {
     fetchTournamentData()
   }, [])
 
+  function displayJoinPopup() {
+    document.querySelector('.joinPopup').style.display = 'flex'
+  }
+
+  function hideJoinPopup() {
+    document.querySelector('.joinPopup').style.display = 'none'
+  }
+
   let button
 
   if(!isHost) {
@@ -54,7 +70,7 @@ function Tournament() {
     else if(!tournament.hasStarted && !applicationAccepted) {
       button = <button className="btn btn-secondary" onClick={handleApply}>Apply</button>
     }
-    else if(!tournament.hasStarted) {
+    else if(!tournament.hasStarted && applicationAccepted) {
       button = <button className="btn btn-primary" onClick={handleJoin}>Join</button>
     }
     else {
@@ -65,48 +81,88 @@ function Tournament() {
     button = <button className="btn btn-manage" onClick={() => console.log("Manage tournament")}>Manage</button>
   }
 
+  function SoloPopup() {
+    return (
+      <div className="joinPopup">
+        <img onClick={hideJoinPopup} src={alpha_x} alt="" />
+        <h1>Join Tournament</h1>
+        <div className="joinPopup-details">
+          Are you sure you want to join this tournament?
+        </div>
+        <button className="btn btn-primary joinPopup-confirm">
+          Confirm
+        </button>
+      </div>
+    )
+  }
+
+  function TeamPopup() {
+    return (
+      <div className="joinPopup">
+        <img onClick={hideJoinPopup} src={alpha_x} alt="" />
+        <h1>Choose Team</h1>
+        <div className="joinPopup-teams">
+          <div>Team 1</div>
+          <div>Team 2</div>
+          <div>Team 3</div>
+          <div>...</div>
+          <div>...</div>
+          <div>...</div>
+          <div>...</div>
+          <div>...</div>
+          <div>...</div>
+          <div>...</div>
+        </div>
+        <button className="btn btn-primary joinPopup-confirm">
+          Confirm
+        </button>
+      </div>
+    )
+  }
+
   return (
     <div id="Tournament">
       <Nav />
-        <div className="tournament-container">
-        { isLoading ? <h1 className="loadingText">Loading tournament ...</h1> : 
-        <>
-          <div className="tournament-info">
-            <div className="tournament-specs">
-              <h1 className="tournament-title">{tournament.title}</h1>
-              <p className="tournament-description">{tournament.description}</p>
-              <p className="tournament-category">Category: {tournament.category.replace(/(^\w|\s\w)/g, m => m.toUpperCase())}</p>
-              <p className="tournament-type">Format: {tournament.type.replace(/(^\w|\s\w)/g, m => m.toUpperCase())}</p>
-              <p className="tournament-teamSize">{tournament.teamSize != 1 ? (`Team size: ${tournament.teamSize} players`) : "Team size: Solo"}</p>
-              <p className="tournament-capacity">Capacity: {tournament.enrolledUsers.length}/{tournament.maxCapacity}</p>
-              <p className="tournament-entryFee">Entry Fee: ${tournament.entryFee}</p>
-              <div className="tournament-earnings">Earnings:
-                <p>1st: ${tournament.earnings[1]}</p>
-                <p>2nd: ${tournament.earnings[2]}</p>
-                <p>3rd: ${tournament.earnings[3]}</p>
-              </div>
-              <p className="tournament-accessibility">{tournament.accessibility != 'open' ? tournament.accessibility.replace(/(^\w|\s\w)/g, m => m.toUpperCase()) : ""}</p>
+      <div className="tournament-container">
+      { isLoading ? <h1 className="loadingText">Loading tournament ...</h1> : 
+      <>
+        <div className="tournament-info">
+          <div className="tournament-specs">
+            <h1 className="tournament-title">{tournament.title}</h1>
+            <p className="tournament-description">{tournament.description}</p>
+            <p className="tournament-category">Category: {tournament.category.replace(/(^\w|\s\w)/g, m => m.toUpperCase())}</p>
+            <p className="tournament-type">Format: {tournament.type.replace(/(^\w|\s\w)/g, m => m.toUpperCase())}</p>
+            <p className="tournament-teamSize">{tournament.teamSize != 1 ? (`Team size: ${tournament.teamSize} players`) : "Team size: Solo"}</p>
+            <p className="tournament-capacity">Capacity: {tournament.enrolledUsers.length}/{tournament.maxCapacity}</p>
+            <p className="tournament-entryFee">Entry Fee: ${tournament.entryFee}</p>
+            <div className="tournament-earnings">Earnings:
+              <p>1st: ${tournament.earnings[1]}</p>
+              <p>2nd: ${tournament.earnings[2]}</p>
+              <p>3rd: ${tournament.earnings[3]}</p>
             </div>
-
-            {button}
+            <p className="tournament-accessibility">{tournament.accessibility != 'open' ? tournament.accessibility.replace(/(^\w|\s\w)/g, m => m.toUpperCase()) : ""}</p>
           </div>
 
-          <div className="tournament-content">
-
-            {tournament.type === "brackets" ? (
-              <div className="brackets">Brackets Display</div>
-            ) : (
-              <div className="battle-royale">Battle Royale List</div>
-            )}
-
-            <div className="tournament-updates">
-              <h3>Updates</h3>
-              {tournament.updates.map((update) => <p key={update.date}>[{(new Date(update.date)).toDateString()}] {update.content}</p>)}
-            </div>
-          </div>
-        </>
-        }
+          {button}
         </div>
+
+        <div className="tournament-content">
+
+          {tournament.type === "brackets" ? (
+            <div className="brackets">Brackets Display</div>
+          ) : (
+            <div className="battle-royale">Battle Royale List</div>
+          )}
+
+          <div className="tournament-updates">
+            <h3>Updates</h3>
+            {tournament.updates.map((update) => <p key={update.date}>[{(new Date(update.date)).toDateString()}] {update.content}</p>)}
+          </div>
+        </div>
+      </>
+      }
+      </div>
+      { tournament.teamSize == 1 ? <SoloPopup /> : <TeamPopup /> }
     </div>
   );
 }
