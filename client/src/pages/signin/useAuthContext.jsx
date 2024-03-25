fea77a0ff5f17c986e66cd88e9af68e6454006c5
jadzeid <150618673+jadzeid@users.noreply.github.com>
2024-03-25 14:01:45 +0200

    Update useAuthContext.jsx

--- client/src/pages/signin/useAuthContext.jsx
+++ client/src/pages/signin/useAuthContext.jsx
@@ -1,10 +1,1 @@
-import { authContext } from "./authContext";
-import { useContext } from "react";
 
-export const useAuthContext = ()=>{
-    const context  = useContext(authContext)
-    if(!context){
-        throw Error('useAuthContext must be used inside anAuthContextProvider')
-    }
-    return context
-}
