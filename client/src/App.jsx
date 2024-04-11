adbec20dee9fc9a057ba3068a0d49f9145f30bcc
jadzeid <jadzeid004@gmail.com>
2024-04-11 15:41:19 +0300

    fonts added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,51 +1,46 @@
-<<<<<<< HEAD
+
 import { Route, Router, Routes } from "react-router-dom";
-=======
+
 import { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 
+import React, { useContext } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import AuthContext from "./context/AuthContext.jsx";
-
->>>>>>> bad2fedd7b36171cb8a6c100e4e4ed5bf2401d55
 import Home from './pages/home/Home';
-import Tournaments from './pages/tournaments/Tournaments'
-import Team from './pages/team/Team'
-import ViewTeam from './pages/team/view/View'
+import Tournaments from './pages/tournaments/Tournaments';
+import Team from './pages/team/Team';
+import ViewTeam from './pages/team/view/View';
 import Signin from "./pages/signin/Signin";
 import Signup from "./pages/signup/Signup";
-import Tournament from './pages/tournament/Tournament'
-<<<<<<< HEAD
-import Host from './pages/host/host';
+import Tournament from './pages/tournament/Tournament';
+import Profile from './pages/profile/Profile.jsx'; // Added from dev branch
+import Host from './pages/host/host'; // Added from dev branch
 
-=======
-import Profile from './pages/profile/Profile.jsx'
->>>>>>> bad2fedd7b36171cb8a6c100e4e4ed5bf2401d55
-
-import '/src/styles/index.css'
+import '/src/styles/index.css';
 
 function App() {
-
-  const { loggedIn } = useContext(AuthContext)
+  const { loggedIn } = useContext(AuthContext);
 
   return (
     <Routes>
-      { loggedIn === false &&
+      {loggedIn === false && (
         <>
-        <Route path='/signin' element={<Signin />} />
-        <Route path='/signup' element={<Signup />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/signup' element={<Signup />} />
         </>
-      }
-      { loggedIn === true &&
+      )}
+      {loggedIn === true && (
         <>
-        <Route path='/profile' element={<Profile />} />
+          <Route path='/profile' element={<Profile />} />
         </>
-      }
+      )}
       <Route path='/' element={<Home />} />
       <Route path='/tournaments' element={<Tournaments />} />
       <Route path='/team' element={<Team />} />
       <Route path='/team/view' element={<ViewTeam />} />
       <Route path='/tournament' element={<Tournament />} />
-      <Route path='/host' element={<Host/>} />
+      <Route path='/host' element={<Host />} />
     </Routes>
   );
 }
