38edb5e77a431f3ea863d350a467eb762832275c
Haytham <haythamduwaji@gmail.com>
2024-04-12 18:58:12 +0300

    hi

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -63,7 +63,7 @@ const loggedIn = async (req, res) => {
     if(!token) return res.json(false)
 
     jwt.verify(token, process.env.SECRET)
-
+    console.log('Token:', data.token);
     res.send(true)
   } catch(error) {
     res.json(false)
