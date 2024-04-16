8682d504963899cf6c96ac4e21c6fd93d15bf139
Haytham <haythamduwaji@gmail.com>
2024-04-16 12:57:20 +0300

    hi

--- server/routes/teamRoutes.js
+++ server/routes/teamRoutes.js
@@ -18,10 +18,10 @@ const checkTeamMembership = require("../middleware/checkMember");
 
 // Routes
 router.post("/create", requireAuth, createTeam); // Create a new team
-router.get('/user' ,requireAuth, getTeamsByUser);
+router.get('/user' ,getTeamsByUser);
 router.get("/view/:UUID", requireAuth, getTeam); // Get specific team info, checks membership
 router.get("/view/:UUID/members", checkTeamMembership, getTeamMembers); // Get list of team members, checks membership
-router.post("/join", joinTeam); // Join a team by ID
+router.post("/team/join/:teamId", requireAuth,joinTeam); // Join a team by ID
 router.post("/changeLeader", checkTeamMembership, changeLeader); // Change team leader, checks membership
 router.post("/kick", checkTeamMembership, kickMember); // Kick a member from the team, checks membership
 router.delete("/delete/:UUID", requireAuth, deleteTeam); // Delete a team, checks membership
