61d1c0013689387f0ef87bae35de48b5a71a5208
Haytham <haythamduwaji@gmail.com>
2024-04-22 21:28:50 +0300

    updated routes

--- server/routes/tourneyRoutes.js
+++ server/routes/tourneyRoutes.js
@@ -1,40 +1,59 @@
 const express = require('express');
-const {createTournament, getTournamentById, getAllTournaments, updateTournament, deleteTournament, getTournamentDisplayData, handleApplicationSubmission, handleJoinAsSolo, handleJoinAsTeam, editTitle, editDescription, editStartDate, editEndDate, getManageTournamentDisplayData,getTrendingTournaments, getTournamentCategories} = require('../controller/tourneyController');
+const {createTournament, getTournamentById, getAllTournaments, updateTournament, deleteTournament, getTournamentDisplayData, handleApplicationSubmission, handleJoinAsSolo, handleJoinAsTeam, editTitle, editDescription, editStartDate, editEndDate, getManageTournamentDisplayData,getTrendingTournaments, getTournamentCategories,response,updateScore} = require('../controller/tourneyController');
 const router = express();
 const { auth, getAuth } = require("../middleware/requireAuth")
+
 
 //tournaments
 router.get('/', auth, getAllTournaments);
 
+
 //new tourney
 router.post('/', auth, createTournament);
 
+
 router.get('/tournament', getAuth, getTournamentDisplayData)
+
 
 router.get('/tournament/manage', auth, getManageTournamentDisplayData)
 
+
 router.post('/tournament/submitApplication', auth, handleApplicationSubmission)
+
 
 router.post('/tournament/joinAsSolo', auth, handleJoinAsSolo)
 
+
 router.post('/tournament/joinAsTeam', auth, handleJoinAsTeam)
+
 
 router.post('/tournament/editTitle', auth, editTitle)
 
+
 router.post('/tournament/editDescription', auth, editDescription)
+
 
 router.post('/tournament/editStartDate', auth, editStartDate)
 
+
 router.post('/tournament/editEndDate', auth, editEndDate)
+
 
 router.get('/getTrendingTournaments', getTrendingTournaments)
 
+
 router.get('/getTournamentCategories', getTournamentCategories)
+
+
+router.post('/updateScore', updateScore)
+
 
 //delete
 router.delete('/',deleteTournament);
 
+
 //update
 router.patch('/',updateTournament);
+
 
 module.exports = router;
